fc6587cd229b5342d854ca2ea4bcfc8dcf393286
Gary Hughes <gary.hughes87@virginmedia.com>
2021-01-25 11:52:46 +0000

    Added tests in mysql-from-python.py

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import pymysql
 
 # Get username from GitPod workspace
@@ -13,10 +14,17 @@ connection = pymysql.connect(host='localhost',
 try:
     # Run a query
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        # sql = "SELECT * FROM Genre;"
+        rows = [("Bob", 21, "1990-02-06 23:04:56"),
+                ("Jim", 56, "1955-05-09 13:12:45"),
+                ("Fred", 100, "1911-09-12 01:01:01")]
+        cursor.executemany("INSERT INTO Friends VALUES (%s, %s, %s);", rows)
+        connection.commit()
+        # Note that the above will still display a warning (not error) if the
+        # table already exists
+        # result = cursor.fetchall()
+        # for row in cursor:
+            # print(row)
 finally:
     # Close the connection, regardless of whether the above was successful
     connection.close()
